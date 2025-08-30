500ef830809131da2a82247c8150cf4c66560593
Yashvi-hosteller <yashvi@thehosteller.com>
2025-08-30 16:34:28 +0530

    empty dart project init

---
+++ bin/kata_calc_tdd.dart
@@ -0,0 +1,5 @@
+import 'package:kata_calc_tdd/kata_calc_tdd.dart' as kata_calc_tdd;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kata_calc_tdd.calculate()}!');
+}
---
+++ test/kata_calc_tdd_test.dart
@@ -0,0 +1,8 @@
+import 'package:kata_calc_tdd/kata_calc_tdd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
