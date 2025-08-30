bbbe15b3620c69cfb59385d2eda1973ce7c2effb
Yash Bhansali <yashbhansali55@gmail.com>
2025-08-30 17:05:49 +0530

    handling single number input cases

--- lib/kata_calc_tdd.dart
+++ lib/kata_calc_tdd.dart
@@ -2,7 +2,10 @@ class KataCalculator {
   static int add(String inputNumber) {
     // Returning 0 if the given input is Empty
     if (inputNumber.isEmpty) return 0;
-
-    return 0;
+    try {
+      return int.parse(inputNumber);
+    } catch (e) {
+      throw Exception("Given $inputNumber input it not a valid number");
+    }
   }
 }
--- test/kata_calc_tdd_test.dart
+++ test/kata_calc_tdd_test.dart
@@ -3,8 +3,31 @@ import 'package:test/test.dart';
 
 void main() {
   group("kata calculator add test cases", () {
-    test('return 0 for empty input number', () {
+    test('return 0 for empty input string', () {
       expect(KataCalculator.add(""), 0);
+    });
+
+    test('returns 1 for single input number: 1', () {
+      expect(KataCalculator.add("1"), 1);
+    });
+
+    test('returns 4 for single input number: 4', () {
+      expect(KataCalculator.add("4"), 4);
+    });
+
+    test('throws not a number error for input: ABC', () {
+      String input = 'ABC';
+      expect(
+        () => KataCalculator.add(input),
+        throwsA(
+          predicate((e) {
+            return e is Exception &&
+                e.toString().contains(
+                  'Given $input input it not a valid number',
+                );
+          }),
+        ),
+      );
     });
   });
 }
